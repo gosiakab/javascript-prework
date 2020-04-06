1e90df3aba308174e078bd4f44589985123a7663
Gosia Kabaczynska <mkabaczynska@wp.pl>
2020-04-06 19:20:01 +0100

    add files

---
+++ script.js
@@ -0,0 +1,65 @@
+let computerMove = "kamien";
+let playerMove = "papier";
+
+function printMessage(msg) {
+  let div = document.createElement("div");
+  div.innerHTML = msg;
+  document.getElementById("messages").appendChild(div);
+}
+
+function clearMessages() {
+  document.getElementById("messages").innerHTML = "";
+}
+
+printMessage(
+  " Zagrałem " +
+    computerMove +
+    "! Jeśli Twój ruch to " +
+    playerMove +
+    ",to wygrywasz!"
+);
+let randomFraction = Math.random();
+printMessage(`Wylosowany ulamek to: ${randomFraction}`);
+let calculation = randomFraction * 3 + 1;
+printMessage("Ulamek pomnozony przez 3 i powiekszony o 1: " + calculation);
+let roundNumber = Math.floor(calculation);
+printMessage("Liczba po zaokragleniu w dol to:" + roundNumber);
+let randomNumber = Math.floor(Math.random() * 3 + 1);
+console.log("Wylosowana liczba to: " + randomNumber);
+
+if (randomNumber == 1) {
+  computerMove = "kamien";
+
+  printMessage("Moj ruch to: " + computerMove);
+} else if (randomNumber == 2) {
+  computerMove = "papier";
+  printMessage("Moj ruch to: " + computerMove);
+} else if (randomNumber == 3) {
+  computerMove = "nozyce";
+  printMessage("Moj ruch to: " + computerMove);
+}
+
+let playerInput = prompt("Wybierz swoj ruch! 1: kamien, 2: papier, 3: nozyce.");
+console.log("Gracz wpisal: " + playerInput);
+
+if (playerInput == "1") {
+  playerMove = "kamien";
+} else if (playerInput == "2") {
+  playerMove = "papier";
+} else if (playerInput == "3") {
+  playerMove = "nozyce";
+} else if (playerInput > "3") {
+  playerMove = "nieznany ruch";
+}
+
+printMessage(" Twoj ruch to: " + playerMove);
+
+if (computerMove == "kamien" && playerMove == "papier") {
+  printMessage("Ty wygrywasz!");
+} else if (computerMove == "kamien" && playerMove == "nozyce") {
+  printMessage("Ty przegrywasz!");
+} else if (computerMove == "kamien" && playerMove == "kamien") {
+  printMessage("Remisujesz!");
+} else if (computerMove == "kamien" && playerMove > "nieznany ruch") {
+  printMessage("Wybrales nieznany ruch!");
+}
