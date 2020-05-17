c973d119b48ee3338b2ac0f9ad2806324776d003
Gosia Kabaczynska <mkabaczynska@wp.pl>
2020-05-17 10:36:47 +0100

    display result correction

--- script.js
+++ script.js
@@ -10,25 +10,15 @@ let randomNumber = Math.floor(Math.random() * 3 + 1);
 let computerMove = getMoveName(randomNumber);
 let playerInput = prompt("Wybierz swoj ruch! 1: kamien, 2: papier, 3: nozyce.");
 let playerMove = getMoveName(playerInput);
-function getMoveName(computerMove) {
-  if (computerMove == 1) {
+function getMoveName(move) {
+  if (move == 1) {
     return "kamien";
-  } else if (computerMove == 2) {
+  } else if (move == 2) {
     return "papier";
-  } else if (computerMove == 3) {
-    return "nozyce";
-  }
-}
-function getMoveName(playerMove) {
-  if (playerMove == 1) {
-    return "kamien";
-  } else if (playerMove == 2) {
-    return "papier";
-  } else if (playerMove == 3) {
+  } else if (move == 3) {
     return "nozyce";
   } else {
-    printMessage("Nie znam ruchu o nazwie " + playerMove + ".");
-    return "nieznany ruch";
+    return "nieznany ruch!";
   }
 }
 function displayResult(computerMove, playerMove) {
